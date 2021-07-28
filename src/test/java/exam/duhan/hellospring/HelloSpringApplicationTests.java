cc239eee73fbc1e4306c01a8a05f9a48d9027ad4
Duhanmo <devduhanmo@gmail.com>
2021-07-28 12:58:43 +0900

    first commit

---
+++ src/test/java/exam/duhan/hellospring/HelloSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package exam.duhan.hellospring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HelloSpringApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
