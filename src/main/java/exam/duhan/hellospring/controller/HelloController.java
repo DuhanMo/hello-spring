b751e114dae9dee50f7e52395f13a0c2cf1971e5
DuhanMo <devduhanmo@gmail.com>
2021-07-28 22:29:29 +0900

    commit

--- src/main/java/exam/duhan/hellospring/controller/HelloController.java
+++ src/main/java/exam/duhan/hellospring/controller/HelloController.java
@@ -4,6 +4,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class HelloController {
@@ -15,5 +16,36 @@ public class HelloController {
 
 
         return "hello";
+
     }
+
+    @GetMapping("hello")
+    @ResponseBody
+    public String helloSpring(@RequestParam(value = "name", required = false) String name) {
+        return "hello" + name;
+    }
+
+    @GetMapping("hello-api")
+    @ResponseBody
+    public Hello helloApi(@RequestParam("name") String name) {
+        Hello hello = new Hello();
+        hello.setName(name);
+        return hello;
+    }
+
+    static class Hello {
+
+        private String name;
+
+
+        public String getName() {
+            return name;
+        }
+
+        public void setName(String name) {
+            this.name = name;
+        }
+    }
+
+
 }
