4f2552545a485bab5885f83db06a5d2cb92ae932
Duhanmo <devduhanmo@gmail.com>
2021-08-03 10:05:42 +0900

    jpa 테스트 완료

--- src/main/java/exam/duhan/hellospring/config/SpringConfig.java
+++ src/main/java/exam/duhan/hellospring/config/SpringConfig.java
@@ -1,15 +1,28 @@
 package exam.duhan.hellospring.config;
 
 
+import exam.duhan.hellospring.repository.JpaMemberRepository;
 import exam.duhan.hellospring.repository.MemberRepository;
 import exam.duhan.hellospring.repository.MemoryMemberRepository;
 import exam.duhan.hellospring.service.MemberService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.stereotype.Component;
 
+import javax.persistence.EntityManager;
+import javax.sql.DataSource;
+
 @Configuration
 public class SpringConfig {
+
+    private final DataSource datasource;
+    private final EntityManager em;
+
+    public SpringConfig(DataSource datasource, EntityManager em) {
+        this.datasource = datasource;
+        this.em = em;
+    }
 
     @Bean
     public MemberService memberService() {
@@ -18,6 +31,6 @@ public class SpringConfig {
 
     @Bean
     public MemberRepository memberRepository() {
-        return new MemoryMemberRepository();
+        return new JpaMemberRepository(em);
     }
 }
