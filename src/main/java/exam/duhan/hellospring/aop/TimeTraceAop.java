250e28407ad54fc65cced03b350404863897333f
Duhanmo <devduhanmo@gmail.com>
2021-08-06 08:37:02 +0900

    AOP 작성

---
+++ src/main/java/exam/duhan/hellospring/aop/TimeTraceAop.java
@@ -0,0 +1,25 @@
+package exam.duhan.hellospring.aop;
+
+import org.aspectj.lang.ProceedingJoinPoint;
+import org.aspectj.lang.annotation.Around;
+import org.aspectj.lang.annotation.Aspect;
+import org.springframework.stereotype.Component;
+
+@Component
+@Aspect
+public class TimeTraceAop {
+
+    @Around("execution(* exam.duhan.hellospring..*(..))")
+    public Object execute(ProceedingJoinPoint joinPoint) throws Throwable {
+        long start = System.currentTimeMillis();
+        System.out.println("start  : " + joinPoint.toString());
+
+        try {
+            return joinPoint.proceed();
+        } finally {
+            long finish = System.currentTimeMillis();
+            long timeMs = finish - start;
+            System.out.println("end  : " + joinPoint.toString() + " " + timeMs + "ms");
+        }
+    }
+}
